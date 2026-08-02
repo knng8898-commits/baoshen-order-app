920175d4b2c33ed4ad99c42cd0c191d02788be38
knng8898-commits <knng8898@gmail.com>
2026-08-03 02:57:57 +0800

    新增總部建議庫存

--- data.js
+++ data.js
@@ -27,6 +27,21 @@ const APP_DATA = {
         {name:"高麗菜",unit:"斤",weekday:50,holiday:70},{name:"泡菜",unit:"斤",weekday:1,holiday:1},{name:"素高湯(大)",unit:"罐",weekday:1,holiday:1},{name:"素乾金針",unit:"斤",weekday:4,holiday:4},{name:"素高麗菜乾",unit:"斤",weekday:4,holiday:4},{name:"素香菇絲",unit:"斤",weekday:4,holiday:4},{name:"小乾香菇",unit:"斤",weekday:4,holiday:4},{name:"鳥蛋",unit:"斤",weekday:4,holiday:4},{name:"青菜",unit:"斤",weekday:10,holiday:14},{name:"玉米筍",unit:"盒",weekday:25,holiday:35},{name:"鴨血",unit:"塊",weekday:5,holiday:6},{name:"大黃瓜",unit:"斤",weekday:6,holiday:9},{name:"南瓜",unit:"斤",weekday:6,holiday:10},{name:"白蘿蔔",unit:"斤",weekday:6,holiday:8},{name:"山粉圓",unit:"包",weekday:1,holiday:1},{name:"蒟蒻絲",unit:"件",weekday:1,holiday:1},{name:"豆皮卷(1袋5小包)",unit:"袋",weekday:1,holiday:1},{name:"海帶結",unit:"斤",weekday:3,holiday:4},{name:"豆干",unit:"斤",weekday:3,holiday:3},{name:"素火腿",unit:"條",weekday:1,holiday:1},{name:"素火鍋料",unit:"包",weekday:10,holiday:10},{name:"台一三角豆腐",unit:"盒",weekday:1,holiday:2},{name:"白毛肚",unit:"斤",weekday:2,holiday:2},{name:"黑毛肚",unit:"斤",weekday:1,holiday:1},{name:"魚下巴",unit:"個",weekday:6,holiday:6},{name:"排骨酥",unit:"斤",weekday:3,holiday:3},{name:"蟹肉棒",unit:"包",weekday:1,holiday:1},{name:"蛋餃",unit:"包",weekday:1,holiday:1},{name:"金利華魚包蛋",unit:"包",weekday:1,holiday:1},{name:"福茂大貢丸",unit:"包",weekday:1,holiday:1},{name:"豬血糕(1包5小片)",unit:"包",weekday:1,holiday:1},{name:"鮮蚵(乾)",unit:"包",weekday:3,holiday:4},{name:"蛤蜊(大)",unit:"斤",weekday:6,holiday:12},{name:"豆腐",unit:"板",weekday:1,holiday:2},{name:"薑絲",unit:"包",weekday:2,holiday:3},{name:"小豆苗",unit:"斤",weekday:2,holiday:3},{name:"麵條(5斤)",unit:"包",weekday:2,holiday:3},{name:"青蔥",unit:"包",weekday:3,holiday:4},{name:"蒜泥",unit:"包",weekday:4,holiday:5},{name:"蘿蔔泥",unit:"包",weekday:3,holiday:4},{name:"去頭朝天辣椒",unit:"斤",weekday:2,holiday:2},{name:"仙草",unit:"桶",weekday:1,holiday:1},{name:"絞肉(綜合)",unit:"斤",weekday:6,holiday:6},{name:"杏鮑菇",unit:"斤",weekday:3,holiday:3},{name:"金針菇",unit:"包",weekday:3,holiday:3},{name:"木耳",unit:"斤",weekday:10,holiday:10},{name:"香菇",unit:"斤",weekday:1,holiday:1},{name:"整支青蔥",unit:"斤",weekday:0,holiday:0},{name:"肥絞肉",unit:"斤",weekday:0,holiday:0}
       ]}]},
     "客惟您": {rule:"customerice",header:"store",footer:"謝謝",groups:[{title:"冰淇淋",items:["香草","草莓","巧克力","香芋","花生","鳳梨","瑞士巧克力","薄荷巧克力","香檳葡萄","紅豆牛奶","芒果","青蘋果","山藥","咖啡","芝麻","牛奶雪沙","芭樂","紫莓優格","哈密瓜","榴槤","布丁彩虹","情人果"].map(name=>({name,unit:"桶"}))}]},
-    "總部": {rule:"headquarters",header:"tomorrow",footer:"謝謝",groups:[{title:"總部品項",items:[{name:"大骨",unit:"桶"},{name:"醬油",unit:"桶"},{name:"高湯粉 全",unit:"箱"},{name:"高湯粉1/4",unit:"箱"},{name:"高湯粉1/2",unit:"箱"},{name:"養生包",unit:"包"},{name:"麻辣香粉",unit:"包"}]}]}
-  }
+"總部": {
+  rule: "headquarters",
+  header: "tomorrow",
+  footer: "謝謝",
+  groups: [{
+    title: "總部品項",
+    items: [
+      { name: "大骨", unit: "桶", weekday: 2, holiday: 2 },
+      { name: "醬油", unit: "桶", weekday: 5, holiday: 5 },
+      { name: "高湯粉 全", unit: "箱", weekday: 1, holiday: 1 },
+      { name: "高湯粉1/4", unit: "箱", weekday: 1, holiday: 1 },
+      { name: "高湯粉1/2", unit: "箱", weekday: 1, holiday: 1 },
+      { name: "養生包", unit: "包", weekday: 2, holiday: 2 },
+      { name: "麻辣香粉", unit: "包", weekday: 1, holiday: 1 }
+    ]
+  }]
+}  }
 };
